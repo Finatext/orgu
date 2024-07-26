49a5e10e126fedf2340fe1e7b1d28d90136f44f2
Taiki Ono <taiki45@users.noreply.github.com>
2024-07-26 16:08:23 +0900

    Update Homebrew formula (#22)
    Signed-off-by: Taiki Ono <taiki@finatext.com>

--- HomebrewFormula/orgu.rb
+++ HomebrewFormula/orgu.rb
@@ -4,13 +4,13 @@
 class Orgu < Formula
   desc "orgu is a tool for implementing organization-wide workflows on GitHub"
   homepage "https://github.com/Finatext/orgu"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Finatext/orgu/releases/download/v0.1.1/orgu-aarch64-apple-darwin.tar.gz"
-      sha256 "2b088b05af67cf334ba3279de6d37dfa5c1bc70175968a22d903061f780fc66d"
+      url "https://github.com/Finatext/orgu/releases/download/v0.1.2/orgu-aarch64-apple-darwin.tar.gz"
+      sha256 "c26af6cf3b8e73ed3a26e822da9cdb63ca7e24f9f01319466cc2f44089cdb27d"
 
       def install
         bin.install "orgu"
@@ -18,8 +18,8 @@ class Orgu < Formula
     end
 
     on_intel do
-      url "https://github.com/Finatext/orgu/releases/download/v0.1.1/orgu-x86_64-apple-darwin.tar.gz"
-      sha256 "360e42f90da281695ab8e9832c39e80a665dfcc8e0b2fff98cda9b8f7704a554"
+      url "https://github.com/Finatext/orgu/releases/download/v0.1.2/orgu-x86_64-apple-darwin.tar.gz"
+      sha256 "239314e0a6b041ae0e24bd6cdc0939ded67cb6a6f2d9550707385648b13773ba"
 
       def install
         bin.install "orgu"
@@ -29,8 +29,8 @@ class Orgu < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/Finatext/orgu/releases/download/v0.1.1/orgu-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "79bb153fa98999a57f7634d1b607f8922b36a96527abd062b59c2ac1cbc4c3de"
+      url "https://github.com/Finatext/orgu/releases/download/v0.1.2/orgu-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "2a37c0f932295e9c88dddea7148360d354828b0914b8fb61ba4f8a3bb38b986e"
 
       def install
         bin.install "orgu"
