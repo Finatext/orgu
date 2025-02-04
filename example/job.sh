c84653d081cc3e1211b43f206af911feaa5911dc
Taiki Ono <taiki45@users.noreply.github.com>
2025-02-04 14:49:36 +0900

    Move to canical gitleaks in example (#77)
    Signed-off-by: Taiki Ono <taiki@finatext.com>

--- example/job.sh
+++ example/job.sh
@@ -6,7 +6,10 @@ TOOL_PATH=${TOOL_PATH:-/usr/src/}
 mkdir -p tmp
 
 report_path=tmp/report.json
-gitleaks detect --no-color --no-banner --exit-code=0 --verbose -r "${report_path}" -c "${TOOL_PATH}/scan_config.toml"
+gitleaks git --no-color --no-banner --exit-code=0 --verbose \
+  --report-format template --report-template "${TOOL_PATH}/jsonextra.json.tmpl" \
+  --report-path "${report_path}" -c "${TOOL_PATH}/scan_config.toml"
+
 gls apply --no-fail -c "${TOOL_PATH}/empty_allowlist.toml" -r "${report_path}" -f sarif | \
   reviewdog -f=sarif -fail-on-error -name="${JOB_NAME}" -reporter=github-pr-review || \
   # Send notification etc.
